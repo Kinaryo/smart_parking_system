de40cbb9b72c62acd2b646b6d7269ae93b348f6e
Kinaryo <kinaryocitrahuda@gmail.com>
2026-04-25 08:30:42 +0900

    update env

--- resources/views/Layouts/user-app.blade.php
+++ resources/views/Layouts/user-app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>@yield('title', 'Smart Parking')</title>
+    <title>@yield('title', 'Smart Parking System')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="{{ asset('favicon.ico') }}">
     <link rel="apple-touch-icon" href="{{ asset('favicon.ico') }}">
